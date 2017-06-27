67edfb8756add9476dead89bbcaec20586a14e40
stanley1228 <stanleyfreeze@gmail.com>
2017-06-28 01:06:18 +0800

    加入第3軸判斷旋轉方向

--- Test_7DOF_compare_proj_IK_test.m
+++ Test_7DOF_compare_proj_IK_test.m
@@ -33,9 +33,9 @@ theta1=acos(V_r_hIK'*V_r/(norm(V_r_hIK)*norm(V_r)))
 theta1=theta1*(180)/pi
 %}
  
- %產生正方形O(60,0,0)  Q(80,-20,0)  R(80,-20,-20)  S(60,0,-20)
+
  %把此路徑分成90份
-O_R=[500 -50 0];
+O_R=[300 100 -100];
 Q_R=[500 -200 0];
 R_R=[500 -200 -220];
 S_R=[500 -50 -220];
@@ -45,7 +45,7 @@ S_R=[500 -50 -220];
 % R_L=[500 200 -220];
 % S_L=[500 50 -220];
 
-O_L=[500 -50 0];
+O_L=[300 100 -100];
 Q_L=[500 -200 0];
 R_L=[500 -200 -220];
 S_L=[500 -50 -220];
@@ -83,7 +83,7 @@ end
 %         Path(t,1:3)=O+(Q-O)*(t-1)/DEF_DESCRETE_POINT;
 %  end
 
-for t=1:1:DEF_DESCRETE_POINT
+% for t=1:1:DEF_DESCRETE_POINT
  
     %輸入參數
     in_x_end_R=Path_R(t,1);
@@ -102,8 +102,8 @@ for t=1:1:DEF_DESCRETE_POINT
     in_beta_L=0*(t/DEF_DESCRETE_POINT)*(pi/180);
     in_gamma_L=0*(t/DEF_DESCRETE_POINT)*(pi/180);
 
-    Rednt_alpha_R=(90)*(pi/180);
-    Rednt_alpha_L=(90)*(pi/180);
+    Rednt_alpha_R=(45)*(pi/180);
+    Rednt_alpha_L=(45)*(pi/180);
     %輸出參數 initial
     %theta=zeros(1,7);
     
@@ -137,7 +137,7 @@ for t=1:1:DEF_DESCRETE_POINT
     gamma=in_gamma_R;
     Rednt_alpha=Rednt_alpha_R;
     IK_7DOF
-    theta_R=theta;
+    theta_R=theta
   	
     %y_base_L=-L0;%左手 fullbend
     % theta_L=IK_7DOF_FullBend_proj(L0,L1,L2,L3,L4,L5,x_base_L,y_base_L,z_base_L,in_x_end_L,in_y_end_L,in_z_end_L,in_alpha_L,in_beta_L,in_gamma_L,Rednt_alpha_L)
@@ -158,11 +158,16 @@ for t=1:1:DEF_DESCRETE_POINT
     beta=in_beta_L;
     gamma=in_gamma_L;
     Rednt_alpha=Rednt_alpha_L;
+  
     IK_7DOF_FullBend_proj
-    theta_L=theta;
-    %theta_L(7)=0*(pi/180);
-    %theta_L(6)=0*(pi/180);
-    theta_L(3)=-theta_L(3);
+    theta_L=theta
+    %theta_L(1)=0*(pi/180);
+    %theta_L(2)=0*(pi/180);
+    %theta_L(3)=0;
+    %theta_L(4)=0;
+    %theta_L(5)=0;
+    %theta_L(6)=0;
+    %theta_L(7)=0;
     %forward kinematic
     %theta=[0 0 0 0 0 0 0];
     %theta_L(7)=0
@@ -175,14 +180,14 @@ for t=1:1:DEF_DESCRETE_POINT
     
     
     %畫關節點圖
-    Draw_7DOF_compare_point(P_R,RotationM_R,PathPoint_R,P_L,RotationM_L,PathPoint_L,Vproj_end_ru_rf,V_rf_extend,Vn_u_f,V_rf_l4,Vn_rfl4_nuf,Vproj_end_rfl4_nuf,Vn_nuf_rotx6_along_NRfl4Nuf,Vn_WstToEnd_WstToProjEndRfl4Nuf);
+    Draw_7DOF_compare_point(P_R,RotationM_R,PathPoint_R,P_L,RotationM_L,PathPoint_L,Vproj_end_ru_rf,V_rf_extend,Vn_u_f,V_rf_l4,Vn_rfl4_nuf,Vproj_end_rfl4_nuf,Vn_nuf_rotx6_along_NRfl4Nuf,Vn_WstToEnd_WstToProjEndRfl4Nuf,V_n_yrot12);
    
     %記錄每軸角度變化
     PathTheta_R(t,1:7)=theta_R*(180/pi);
     PathTheta_L(t,1:7)=theta_L*(180/pi);
     
-    In_R=[in_x_end_R in_y_end_R in_z_end_R in_alpha_R in_beta_R in_gamma_R];
-    Out_R=[out_x_end_R out_y_end_R out_z_end_R out_alpha_R out_beta_R out_gamma_R];
+    In_R=[in_x_end_R in_y_end_R in_z_end_R in_alpha_R in_beta_R in_gamma_R]
+    Out_R=[out_x_end_R out_y_end_R out_z_end_R out_alpha_R out_beta_R out_gamma_R]
     
     In_L=[in_x_end_L in_y_end_L in_z_end_L in_alpha_L in_beta_L in_gamma_L]
     Out_L=[out_x_end_L out_y_end_L out_z_end_L out_alpha_L out_beta_L out_gamma_L]
@@ -199,7 +204,7 @@ for t=1:1:DEF_DESCRETE_POINT
 %     end
     
     pause(0.1);
-end
+% end
 
  %畫JointAngle
 %  Draw_7DOF_JointAnglePath(PathTheta);
